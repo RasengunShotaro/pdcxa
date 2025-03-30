e9c8ec07bb9b5e46b7f8b96f0d452f1f207f8485
dk-moriha <moriha@daikin.co.jp>
2025-03-30 21:22:24 +0900

    ♻️ 不要なコメントの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
-import { TopBar } from "@/components/elements/TopBar";
-import { AppSidebar as Sidebar } from "@/components/elements/Sidebar";
 import { MainContent } from "@/components/elements/MainContent";
+import { AppSidebar as Sidebar } from "@/components/elements/Sidebar";
+import { Header } from "@/components/elements/TopBar";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-gray-100">
-      {/* トップバー */}
-      <TopBar />
-      
-      <div className="flex pt-16"> {/* pt-16でトップバーの高さ分を確保 */}
-        {/* サイドバー */}
+      <Header />
+      <div className="flex pt-16">
         <div className="flex-none">
           <Sidebar />
         </div>
-        
-        {/* メインコンテンツエリア */}
         <div className="flex-1 p-6 overflow-auto">
           <MainContent />
         </div>
