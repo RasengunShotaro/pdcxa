28bb229bbfac8b9d502dd107d55edf8b9abc09fb
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-02 15:41:29 +0900

    👍 RePDの読み書き部分を作成

---
+++ src/hooks/useRePd.ts
@@ -0,0 +1,42 @@
+"use client";
+
+import {
+  PD_QUERY_KEY,
+  REPD_QUERY_KEY,
+  createRePd,
+  getRePds,
+  updateRePdCache,
+} from "@/feature/pd/api/pd";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export const useRePd = (pdId: string) => {
+  const queryClient = useQueryClient();
+
+  const { data: rePds, error } = useQuery({
+    queryKey: [...PD_QUERY_KEY, pdId],
+    queryFn: async () => {
+      const pds = await getRePds(pdId);
+      return pds;
+    },
+    staleTime: 60 * 1000,
+    gcTime: 5 * 60 * 1000,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
+
+  const { mutate: createNewRePd } = useMutation({
+    mutationFn: (content: string) => createRePd(pdId, content),
+    onSuccess: () => {
+      updateRePdCache(queryClient, pdId);
+      queryClient.invalidateQueries({ queryKey: [...PD_QUERY_KEY, pdId] });
+      queryClient.invalidateQueries({ queryKey: REPD_QUERY_KEY });
+    },
+  });
+
+  return {
+    rePds,
+    error,
+    createRePd: createNewRePd,
+  };
+};
