529b4ff375c11280ced52d5a776b6b64875b8270
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-02 16:22:39 +0900

    👍

---
+++ src/feature/pd/components/PostRePdButton.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { MessageCircle } from "lucide-react";
+import { useState } from "react";
+import RePdModal from "./RePdModal";
+
+type PostRePdButtonProps = {
+  pdId: string;
+};
+
+// PostPdButtonとほぼ一緒なので、あとでDRYするかも
+export function PostRePdButton({ pdId }: PostRePdButtonProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <Button
+        onClick={() => setIsModalOpen(true)}
+        className="fixed bottom-20 right-6 w-14 h-14 rounded-full p-0 shadow-lg"
+        size="icon"
+      >
+        <MessageCircle className="h-4 w-4" />
+      </Button>
+      <RePdModal
+        pdId={pdId}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </>
+  );
+}
