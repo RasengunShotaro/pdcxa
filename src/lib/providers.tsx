f6ff746d80a43f15e0fb6f21b1a4a1b14eb12212
dk-moriha <moriha@daikin.co.jp>
2025-04-11 22:41:03 +0900

    👍 Clerk導入

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,8 +1,10 @@
 "use client";
-
+import { ClerkProvider } from "@clerk/nextjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { type ReactNode, useState } from "react";
+import { jaJP } from "@clerk/localizations";
+import { dark } from '@clerk/themes'
 
 export function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(
@@ -20,11 +22,16 @@ export function Providers({ children }: { children: ReactNode }) {
   );
 
   return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>
-        {children}
-        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-      </QueryClientProvider>
-    </SessionProvider>
+    <ClerkProvider localization={jaJP} 
+    appearance={{
+      baseTheme: dark,
+    }}>
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+        </QueryClientProvider>
+      </SessionProvider>
+    </ClerkProvider>
   );
 }
