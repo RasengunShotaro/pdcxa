1b3b80853529f3193497844b76c6ed72cd6f5578
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-13 00:28:24 +0900

    👍 招待ページを作成

---
+++ src/app/invitation/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormField } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const invitationFormSchema = z.object({
+  mail: z.string().email({
+    message: "メールアドレスの形式が正しくありません",
+  }),
+});
+
+type InvitationFormSchema = z.infer<typeof invitationFormSchema>;
+
+export default function Page() {
+  const form = useForm<InvitationFormSchema>({
+    resolver: zodResolver(invitationFormSchema),
+    defaultValues: {
+      mail: "",
+    },
+  });
+
+  const onSubmit = () => {
+    form.reset();
+  };
+
+  return (
+    <div className="max-w-2xl mx-auto">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle>招待を送信</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="mail"
+                render={({ field }) => (
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium mb-1"
+                    >
+                      メールアドレス
+                    </label>
+                    <Input
+                      id="email"
+                      {...field}
+                      placeholder="abcde@example.com"
+                    />
+                  </div>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                PDCXAの世界に招待
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
