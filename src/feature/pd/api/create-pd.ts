d035021d34e290ea4249f2dc34dbc125b7301a92
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-12 17:57:45 +0900

    👍 PDの読み書き先をDBにした

---
+++ src/feature/pd/api/create-pd.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { pds } from "@/db/schema";
+import { db } from "@/lib/db";
+import type { Pd } from "../types";
+
+export const createPd = async (
+  content: string,
+  userId: string
+): Promise<Pd> => {
+  const newPd = {
+    content,
+    createdAt: new Date(),
+    userId: `${userId}`,
+  };
+
+  try {
+    const [insertedPd] = await db.insert(pds).values(newPd).returning({
+      id: pds.id,
+      content: pds.content,
+      createdAt: pds.createdAt,
+      userId: pds.userId,
+    });
+
+    return insertedPd;
+  } catch (error) {
+    console.error("PDの保存に失敗しました:", error);
+    throw error;
+  }
+};
