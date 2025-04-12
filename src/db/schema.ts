808a7641f228a6be2fd56cfeb72ecef95768a987
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-12 17:55:41 +0900

    ♻️ uuidで採番するように変更

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,15 +1,16 @@
-import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import { uuidv7 } from "uuidv7";
 
 export const pds = pgTable("pds", {
-  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  id: uuid("id").primaryKey().$defaultFn(uuidv7),
   content: varchar("content").notNull(),
   createdAt: timestamp("created_at").notNull(),
   userId: varchar("user_id").notNull(),
 });
 
 export const rePds = pgTable("repds", {
-  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
-  pdId: integer("pd_id")
+  id: uuid("id").primaryKey().$defaultFn(uuidv7),
+  pdId: uuid("pd_id")
     .notNull()
     .references(() => pds.id),
   content: varchar("content").notNull(),
@@ -18,7 +19,7 @@ export const rePds = pgTable("repds", {
 });
 
 export const pdLikes = pgTable("pd_likes", {
-  targetPdId: integer("target_pd_id")
+  targetPdId: uuid("target_pd_id")
     .notNull()
     .references(() => pds.id)
     .primaryKey(),
@@ -26,7 +27,7 @@ export const pdLikes = pgTable("pd_likes", {
 });
 
 export const rePdLikes = pgTable("repd_likes", {
-  targetRePdId: integer("target_repd_id")
+  targetRePdId: uuid("target_repd_id")
     .notNull()
     .references(() => rePds.id)
     .primaryKey(),
