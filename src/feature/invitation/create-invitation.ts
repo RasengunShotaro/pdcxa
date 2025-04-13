8647914f53f97a70abaa93d03e4ec90a94c7880c
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-13 11:10:08 +0900

    👍 エラーハンドリング

--- src/feature/invitation/create-invitation.ts
+++ src/feature/invitation/create-invitation.ts
@@ -5,6 +5,7 @@ import { clerkClient } from "@/lib/clerk";
 export const createInvitation = async (email: string) => {
   const result = await clerkClient.invitations.createInvitation({
     emailAddress: email,
+    ignoreExisting: true,
   });
   return result.raw;
 };
