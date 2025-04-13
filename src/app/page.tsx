d176980be94e39c40123ff53932b18f1730ba027
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-13 23:56:18 +0900

    ♻️

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { TimeLine } from "@/components/elements/TimeLine";
+import { PdTimeLine } from "@/components/elements/pd-timeline";
 export const runtime = "edge";
 
 export default function Page() {
-  return <TimeLine />;
+  return <PdTimeLine />;
 }
---
+++ src/components/elements/pd-timeline.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import PdItem from "@/feature/pd/components/Pd/PdItem";
+import { PostPdButton } from "@/feature/pd/components/Pd/PostPdButton";
+import { usePd } from "@/hooks/usePd";
+
+export function PdTimeLine() {
+  const { pds } = usePd();
+
+  return (
+    <div className="max-w-2xl mx-auto">
+      <div className="space-y-4">
+        {pds.map((post) => (
+          <PdItem key={post.id} pd={post} />
+        ))}
+      </div>
+      <PostPdButton />
+    </div>
+  );
+}
