34c7098ff16306eaa709376f01b7ac5440b6da05
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-13 19:41:03 +0900

    👍

--- src/components/elements/TopBar.tsx
+++ src/components/elements/TopBar.tsx
@@ -7,7 +7,7 @@ type HeaderProps = {
 
 export function Header({ children }: HeaderProps) {
   return (
-    <header className="fixed top-0 left-0 right-0 bg-white h-16 px-6 flex items-center justify-between border-b z-50">
+    <header className="sticky top-0 left-0 right-0 bg-white min-h-12 max-h-16 px-6 flex items-center justify-between border-b z-50">
       <div className="flex items-center space-x-4">
         <Link href="/">
           <h1 className="text-xl font-bold">PDCXA</h1>
