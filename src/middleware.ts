b5b4c081d74904de36d2d2cbbf8d940ff09c0f88
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-13 18:24:06 +0900

    ♻️

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const matcher = [
-  "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-  "/(api|trpc)(.*)",
-  "/",
-];
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+    "/",
+  ],
+};
 
-const isProtectedRoute = createRouteMatcher(matcher);
+const isProtectedRoute = createRouteMatcher(config.matcher);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req))
-    await auth.protect({
-      unauthenticatedUrl: "https://accounts.pdcxa.com/sign-in",
-    });
+  if (isProtectedRoute(req)) await auth.protect();
 });
-
-export const config = { matcher };
