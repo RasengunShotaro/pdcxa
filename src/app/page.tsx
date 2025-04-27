da904c2e769bce65f65dc53bfcd39517095e8ece
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-27 17:24:24 +0900

    ♻️

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { PdTimeLine } from "@/components/elements/pd-timeline";
+import { PdTimeLine } from "@/feature/pd/components/Pd/pd-timeline";
 export const runtime = "edge";
 
 export default function Page() {
