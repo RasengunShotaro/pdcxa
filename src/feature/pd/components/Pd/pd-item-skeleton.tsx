21ea7b355b1a65994d3c182f6e46369ce0bf1b01
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-04-27 11:32:46 +0900

    👍 スケルトン修正

--- src/feature/pd/components/Pd/pd-item-skeleton.tsx
+++ src/feature/pd/components/Pd/pd-item-skeleton.tsx
@@ -1,4 +1,9 @@
-import { Card, CardHeader } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export const PdItemSkeleton = ({ PD数 }: { PD数: number }) => {
@@ -7,8 +12,8 @@ export const PdItemSkeleton = ({ PD数 }: { PD数: number }) => {
       <div className="space-y-4">
         {Array.from({ length: PD数 }, (_, i) => i).map((要素) => (
           <div key={`skeleton-${要素}`}>
-            <Card >
-              <CardHeader className="p-4 space-y-2">
+            <Card>
+              <CardHeader className="p-4 pt-0 pb-0">
                 <div className="flex justify-between items-start">
                   <div className="flex items-center space-x-3">
                     <Skeleton className="h-10 w-10 rounded-full" />
@@ -19,17 +24,19 @@ export const PdItemSkeleton = ({ PD数 }: { PD数: number }) => {
                   </div>
                   <Skeleton className="h-8 w-8 rounded-md" />
                 </div>
+              </CardHeader>
+              <CardContent className="p-4 pt-2 pb-2">
                 <Skeleton className="h-20 w-full" />
-                <div className="flex items-center justify-between pt-2">
+              </CardContent>
+              <CardFooter className="p-4 pt-0 pb-0">
+                <div className="flex items-center justify-between w-full">
                   <Skeleton className="h-4 w-24" />
-                  <div className="flex items-center">
-                    <div className="flex mr-2">
-                      <Skeleton className="h-8 w-16 rounded-md" />
-                    </div>
+                  <div className="flex items-center space-x-0.5">
+                    <Skeleton className="h-8 w-16 rounded-md" />
                     <Skeleton className="h-8 w-16 rounded-md" />
                   </div>
                 </div>
-              </CardHeader>
+              </CardFooter>
             </Card>
           </div>
         ))}
