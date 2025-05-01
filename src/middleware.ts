609278d237b3e147462e2fc30081f9f51b51dbff
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-05-01 20:57:09 +0900

    👍 ミドルウェア更新

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/signin(.*)"]);
+const isPublicRoute = createRouteMatcher(["/signin"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
