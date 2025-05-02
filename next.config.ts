e4a169a300d6d42fd96f6da5494de34659af22e1
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-05-02 22:56:09 +0900

    ♻️ AvatarをNext/Imageにした

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    remotePatterns: [new URL("https://img.clerk.com/**")],
+  },
+};
+
 export default nextConfig;
