4be7e6135a77ba1f9c9dd53976aea6a59e660985
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-05-03 00:18:18 +0900

    Revert "♻️ AvatarをNext/Imageにした"
    This reverts commit e4a169a300d6d42fd96f6da5494de34659af22e1.

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
-  images: {
-    remotePatterns: [new URL("https://img.clerk.com/**")],
-  },
 };
 
 export default nextConfig;
