1be9b856ed396ddabd9dfa9b3d326c86696b0b61
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-05-03 10:40:43 +0900

    👍 suspense追加

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import { fetchPds } from "@/feature/pd/api/pd/fetch-pds";
+import { PdItemSkeleton } from "@/feature/pd/components/pd/pd-item-skeleton";
 import { PdTimeLine } from "@/feature/pd/components/pd/pd-timeline";
 import {
   HydrationBoundary,
   QueryClient,
   dehydrate,
 } from "@tanstack/react-query";
+import { Suspense } from "react";
 export const runtime = "edge";
 
 export default async function Page() {
@@ -15,8 +17,10 @@ export default async function Page() {
   queryClient.setQueryData(["PD詳細", null, null], pdData);
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <PdTimeLine />
-    </HydrationBoundary>
+    <Suspense fallback={<PdItemSkeleton PD数={5} />}>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <PdTimeLine />
+      </HydrationBoundary>
+    </Suspense>
   );
 }
