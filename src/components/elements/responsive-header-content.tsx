24b9c146199dfeed549799b711711fd7efab24be
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-05-03 18:03:35 +0900

    👍 ヘッダーをRSCにした

---
+++ src/components/elements/responsive-header-content.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useSidebar } from "@/components/ui/sidebar";
+import Image from "next/image";
+import Link from "next/link";
+import pdcxa from "../../../static/pdcxa.svg";
+import { NavBreadcrumb } from "./nav-breadcrumb";
+
+export const ResponsiveHeaderContent = () => {
+  const { isMobile } = useSidebar();
+
+  return isMobile ? (
+    <Link href="/">
+      <Image
+        src={pdcxa}
+        alt="Logo"
+        className="h-7 w-auto dark:invert"
+        quality={100}
+      />
+    </Link>
+  ) : (
+    <NavBreadcrumb />
+  );
+};
