0a3ba7109bae6cfd3634db7c360a15d524ad749e
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-05-05 22:25:35 +0900

    ♻️

--- src/feature/pd/components/repd/repd-like.tsx
+++ src/feature/pd/components/repd/repd-like.tsx
@@ -1,17 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { useRePdLike } from "@/hooks/use-repd-like";
 import { Heart } from "lucide-react";
-import { useTransition } from "react";
 import type { RePd } from "../../types";
 
 export const Like = ({ rePd }: { rePd: RePd }) => {
   const { isLiked, toggleLike } = useRePdLike(rePd);
-  const [isPending, startTransition] = useTransition();
 
   const handleClick = () => {
-    startTransition(async () => {
-      toggleLike();
-    });
+    toggleLike();
   };
 
   return (
@@ -20,7 +16,6 @@ export const Like = ({ rePd }: { rePd: RePd }) => {
       size="sm"
       className={`space-x-1 ${isLiked ? "!text-red-500" : ""}`}
       onClick={handleClick}
-      disabled={isPending}
     >
       <Heart className="h-4 w-4" />
       <span>{rePd.likeCount}</span>
