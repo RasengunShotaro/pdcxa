0f3ce039df66e3442ed2f4edc480790dfe3ff600
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-05-26 21:36:21 +0900

    👍

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,11 +3,18 @@ import { getRequestContext } from "@cloudflare/next-on-pages";
 export const runtime = "edge";
 
 async function handleRequest(request: Request) {
-  const bff = getRequestContext().env.BFF;
+  try {
+    const bff = getRequestContext().env.BFF;
 
-  const res = await bff.add(2, 2);
+    const res = await bff.add(2, 2);
 
-  return res.toString();
+    return Response.json({ res: res });
+  } catch (error) {
+    return new Response(
+      error instanceof Error ? error.message : "不明なエラー",
+      { status: 500 }
+    );
+  }
 }
 
 export async function GET(request: Request) {
