c8cf2469738be870e2c4366ae544d3cefad10f66
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-05-28 00:33:07 +0900

    👍 迷惑メールに関する記述を追加

--- src/app/(main)/invitation/page.tsx
+++ src/app/(main)/invitation/page.tsx
@@ -1,4 +1,10 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { InvitationForm } from "@/feature/invitation/components/invitation-form";
 
 export default function Page() {
@@ -7,6 +13,9 @@ export default function Page() {
       <Card className="w-full">
         <CardHeader>
           <CardTitle>招待を送信</CardTitle>
+          <CardDescription>
+            招待は迷惑メールフォルダに振り分けられる可能性があります。
+          </CardDescription>
         </CardHeader>
         <CardContent className="pt-4">
           <InvitationForm />
