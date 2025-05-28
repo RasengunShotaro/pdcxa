55e847ef43a7867e53ae5546715e46503a66707c
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-05-28 13:29:06 +0900

    🐛

--- bff/src/index.ts
+++ bff/src/index.ts
@@ -2,6 +2,13 @@ import { WorkerEntrypoint } from "cloudflare:workers";
 import { optimizeImage } from "wasm-image-optimization";
 
 export class RpcService extends WorkerEntrypoint {
+  async fetch(request: Request): Promise<Response> {
+    return new Response("直リンク禁止", {
+      status: 403,
+      statusText: "Forbidden",
+    });
+  }
+
   async compress(image: ArrayBuffer, quality: number): Promise<Uint8Array> {
     return (await optimizeImage({
       image: image,
