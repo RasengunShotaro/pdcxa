8f6fba5e697e9e18394fdb330eabd9ac64e03209
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-06-20 00:25:25 +0900

    👍

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,39 @@
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { Hono } from "hono";
+import { vValidator } from "@hono/valibot-validator";
+import { Hono, type MiddlewareHandler } from "hono";
 import { cors } from "hono/cors";
+import * as v from "valibot";
+
+const schema = v.object({
+  emailAddress: v.string(),
+});
+
+export const エラーレスポンスMiddleware: MiddlewareHandler = async (
+  _,
+  next
+) => {
+  try {
+    await next();
+  } catch (err) {
+    if (err instanceof Response) {
+      return err;
+    }
+  }
+};
+
+export const ログイン状態Middleware: MiddlewareHandler = async (c, next) => {
+  const auth = getAuth(c);
+  const userId = auth?.userId;
+  if (!userId) {
+    throw c.json(
+      {
+        message: "ログインしていません。",
+      },
+      400
+    );
+  }
+  await next();
+};
 
 const app = new Hono();
 app.use("*", clerkMiddleware());
@@ -10,34 +43,29 @@ app.use("*", async (c, next) => {
   });
   return corsMiddlewareHandler(c, next);
 });
+app.use("*", エラーレスポンスMiddleware);
+app.use("*", ログイン状態Middleware);
 
-const invitationApp = new Hono().post("/create", async (c) => {
-  const clerkClient = c.get("clerk");
-  const auth = getAuth(c);
+const invitationApp = new Hono().post(
+  "/create",
+  vValidator("json", schema),
+  async (c) => {
+    const clerkClient = c.get("clerk");
 
-  const userId = auth?.userId;
-  if (!userId) {
+    const body = c.req.valid("json");
+    await clerkClient.invitations.createInvitation({
+      emailAddress: body.emailAddress,
+      ignoreExisting: true,
+    });
+
     return c.json(
       {
-        message: "ログインしていないです",
+        message: "招待を作成しました",
       },
-      400
+      200
     );
   }
-
-  const body: { emailAddress: string } = await c.req.raw.json();
-  await clerkClient.invitations.createInvitation({
-    emailAddress: body.emailAddress,
-    ignoreExisting: true,
-  });
-
-  return c.json(
-    {
-      message: "招待を作成しました",
-    },
-    200
-  );
-});
+);
 
 const ルート = app.route("/invitation", invitationApp);
 
