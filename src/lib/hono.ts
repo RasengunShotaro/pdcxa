b9842ff4bb421a7586507081f5e656ff3bb3de61
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-06-20 00:59:50 +0900

    👍

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -4,10 +4,28 @@ import { auth } from "@clerk/nextjs/server";
 import { hc } from "hono/client";
 import type { AppType } from "@/../backend/src/index";
 
+type FetchArgs = Parameters<typeof fetch>;
+type エラー時のレスポンス = {
+  message: string;
+};
+
 const baseUrl = process.env.BASE_URL;
 if (!baseUrl) {
   throw new Error("バックエンドのURLが設定されていません");
 }
+
+const fetcher = async (input: FetchArgs[0], init: FetchArgs[1]) => {
+  const response = await fetch(input, init);
+
+  if (!response.ok) {
+    const json = (await response.json()) as エラー時のレスポンス;
+    const エラーメッセージ = json.message || "不明なエラー";
+
+    throw new Error(`エラーが発生しました: ${エラーメッセージ}`);
+  }
+
+  return response;
+};
 
 export const getClient = async () => {
   const token = await (await auth()).getToken();
@@ -18,7 +36,8 @@ export const getClient = async () => {
   return hc<AppType>(baseUrl, {
     headers: {
       Authorization: `Bearer ${token}`,
-      "content-type": "application/json",
+      "Content-Type": "application/json",
     },
+    fetch: fetcher,
   });
 };
