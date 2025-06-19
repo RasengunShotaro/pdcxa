0c8eff79b990c692b4e8d9a8bf1ab8e9161bc70f
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-06-19 21:36:36 +0900

    👍 バックエンドにpostするようにした

--- src/feature/invitation/create-invitation.ts
+++ src/feature/invitation/create-invitation.ts
@@ -1,15 +1,18 @@
 "use server";
 
-import { clerkClient } from "@/lib/clerk";
+import { getClient } from "@/lib/hono";
 import { actionClient } from "@/lib/safe-action";
 import { invitationFormSchema } from "./types";
 
 export const createInvitation = actionClient
   .inputSchema(invitationFormSchema)
   .action(async ({ parsedInput: { mail } }) => {
-    const result = await clerkClient.invitations.createInvitation({
-      emailAddress: mail,
-      ignoreExisting: true,
+    const client = await getClient();
+    const res = await client.invitation.create.$post({
+      json: {
+        emailAddress: mail,
+      },
     });
-    return result.raw;
+
+    return res;
   });
