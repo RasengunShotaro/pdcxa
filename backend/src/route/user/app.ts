5452136ab42b3af49089c99e822bba3095e859e5
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-06-20 19:40:30 +0900

    👍 ユーザー詳細をutilsに

--- backend/src/route/user/app.ts
+++ backend/src/route/user/app.ts
@@ -1,6 +1,8 @@
 import { vValidator } from "@hono/valibot-validator";
 import { Hono } from "hono";
 import * as v from "valibot";
+import { ユーザー名に紐づくユーザー詳細を取得 } from "./utils/fetch-user-detail";
+import { ユーザーIDに紐づくユーザー詳細一覧を取得 } from "./utils/fetch-user-details";
 
 const userDetailSchema = v.object({
   userName: v.string(),
@@ -13,42 +15,21 @@ export const userApp = new Hono()
   .get("/detail", vValidator("query", userDetailSchema), async (c) => {
     const clerkClient = c.get("clerk");
 
-    const body = c.req.valid("query");
-    const userDetail = (
-      await clerkClient.users.getUserList({ username: [body.userName] })
-    ).data[0];
+    const { userName } = c.req.valid("query");
+    const ユーザー詳細 = await ユーザー名に紐づくユーザー詳細を取得({
+      userName,
+      clerkClient,
+    });
 
-    return c.json(
-      {
-        id: userDetail.id,
-        firstName: userDetail.firstName,
-        lastName: userDetail.lastName,
-        imageUrl: userDetail.imageUrl,
-        userName: userDetail.username,
-      },
-      200
-    );
+    return c.json(ユーザー詳細, 200);
   })
   .get("/details", vValidator("query", userDetailsSchema), async (c) => {
     const clerkClient = c.get("clerk");
 
-    const body = c.req.valid("query");
-    const limit = body.userIds.length < 500 ? body.userIds.length : 500;
-    const result = (
-      await clerkClient.users.getUserList({
-        userId: body.userIds,
-        limit,
-      })
-    ).data;
-
-    const userDetails = result.map((user) => {
-      return {
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        imageUrl: user.imageUrl,
-        userName: user.username,
-      };
+    const { userIds } = c.req.valid("query");
+    const userDetails = await ユーザーIDに紐づくユーザー詳細一覧を取得({
+      userIds,
+      clerkClient,
     });
 
     return c.json(userDetails, 200);
