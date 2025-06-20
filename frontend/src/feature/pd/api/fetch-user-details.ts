71049c8fbe7d33ca48fbb04d96dcb5d5429abf99
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-06-20 22:24:54 +0900

    🐛 GETでクエリパラメータに配列を送るときのバグ修正

--- frontend/src/feature/pd/api/fetch-user-details.ts
+++ frontend/src/feature/pd/api/fetch-user-details.ts
@@ -6,7 +6,7 @@ export const fetchUserDetails = cache(async (userIds: string[]) => {
 
   const response = await client.user.details.$get({
     query: {
-      userIds,
+      userIds: userIds.length > 1 ? userIds : [...userIds, ""], // クエリパラメータ配列の長さが1のときは配列とみなされないため
     },
   });
   const userDetails = await response.json();
