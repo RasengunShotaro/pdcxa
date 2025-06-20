8523e9b0ed78be6d377abde0895f75e5133b08a8
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-06-20 20:43:53 +0900

    👍 モノレポ化に伴い大移動

--- frontend/src/feature/pd/api/pd/s3-utils.ts
+++ frontend/src/feature/pd/api/pd/s3-utils.ts
@@ -1,29 +1,6 @@
 "use server";
 import { getCloudflareContext } from "@opennextjs/cloudflare";
 
-export const uploadToS3 = async ({
-  body,
-  fileName,
-  contentType,
-  extension,
-}: {
-  body: Uint8Array | Blob;
-  fileName: string;
-  contentType: string;
-  extension: string;
-}) => {
-  const fullFileName = `${fileName}.${extension}`;
-
-  const R2 = getCloudflareContext().env.R2;
-  await R2.put(fullFileName, body, {
-    httpMetadata: {
-      contentType,
-    },
-  });
-
-  return fullFileName;
-};
-
 export const getFromS3 = async (fullFileName: string) => {
   const R2 = getCloudflareContext().env.R2;
   const response = await R2.get(fullFileName);
