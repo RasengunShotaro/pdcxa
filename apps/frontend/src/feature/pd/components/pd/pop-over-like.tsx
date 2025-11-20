7256a77ad34eff29c8bbf6475f2832a9844d44b7
RasengunShotaro <scarlet.mori0318@gmail.com>
2025-11-20 14:36:20 +0900

    👍

--- apps/frontend/src/feature/pd/components/pd/pop-over-like.tsx
+++ apps/frontend/src/feature/pd/components/pd/pop-over-like.tsx
@@ -12,12 +12,16 @@ export const PopOverLike = ({ userNames }: PopOverLikeProps) => {
   return (
     <Popover>
       <PopoverTrigger className="text-sm">いいね</PopoverTrigger>
-      <PopoverContent className="flex-col border-2 w-fit">
+      <PopoverContent className="flex-col border-2 w-fit! max-w-[90vw]! overflow-hidden">
         {userNames.length > 0 ? (
           <>
             <div className="text-lg font-bold">いいねしたユーザー</div>
             {userNames.map((userName) => (
-              <div className="text-muted-foreground font-light" key={userName}>
+              <div
+                className="text-muted-foreground font-light truncate"
+                key={userName}
+                title={userName}
+              >
                 {userName}
               </div>
             ))}
